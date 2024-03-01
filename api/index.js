e179f4a1bbf10564d9f1aac3fb2cf9318a11d12c
Hermann Rasch <hermann.rasch93@gmail.com>
2024-03-01 12:51:09 +0100

    Created database and connected to it

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose.connect(process.env.MONGO)
+.then(() => console.log("MongoDB connection successful!"))
+.catch(err => console.log("MongoDB connection error: " + err));
 const app = express();
 
 app.listen(3000, () => {
