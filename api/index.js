6d352d4d070ff4720a7ff90358f54dd63512534f
Hermann Rasch <hermann.rasch93@gmail.com>
2024-03-04 20:32:17 +0100

    Creation of signup route and authentication

--- api/index.js
+++ api/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO)
 .then(() => console.log("MongoDB connection successful!"))
 .catch((err) => console.log("MongoDB connection error: " + err));
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server running in server 3000");
 }
 );
+
+ app.use("/api/user", userRouter);
+ app.use("/api/auth", authRouter);
