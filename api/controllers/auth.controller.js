1970da7843a75609eed372ed50b4fa1a5d6c41e9
Hermann Rasch <hermann.rasch93@gmail.com>
2024-03-06 23:14:08 +0100

    Creation of Sign up Page, correction to the controller & proxy set up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,9 +7,8 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
-    res.status(201).json("User Created successfullly");
+        res.status(201).json({success: true, message: "User Created successfullly"});
     } catch (error) {
         next(error);
     }
-    
-}
+};
