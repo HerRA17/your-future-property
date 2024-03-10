e7b5f9a1ee1892937390d23157d2027dc39d1f58
Hermann Rasch <hermann.rasch93@gmail.com>
2024-03-10 17:26:46 +0100

    Update header & make profile private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fprofile-image&psig=AOvVaw2slp3mknebR41nHe4VGvJ7&ust=1710106173578000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKjagIqQ6IQDFQAAAAAdAAAAABAE"
+        default: "https://depositphotos.com/vector/male-avatar-profile-picture-vector-119675554.html"
     },
 }, { timestamps: true });
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
-import Header from './components/header';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -14,7 +14,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile/>}/>
+        </Route>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
       </Routes>
