8e43cbe40058e22d04da2ad1c1e11fb87588041d
Hermann Rasch <hermann.rasch93@gmail.com>
2024-03-17 22:03:30 +0100

    Add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
     res.json({
@@ -27,5 +28,16 @@ export const updateUser = async (req, res, next) => {
         res.status(200).json(rest); 
     } catch (error) {
         next(error)
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your user account!"));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie("access_token")
+        res.status(200).json("User has been deleted");
+    } catch (error) {
+        next(error);
     }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,8 @@ import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, 
+  deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -74,7 +75,24 @@ export default function Profile() {
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
-  }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch(error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -112,7 +130,7 @@ export default function Profile() {
           p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Update"}</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700">Delete Account</span>
         <span className="text-red-700">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
