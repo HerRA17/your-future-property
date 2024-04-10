7a52478f2f334799eb30051158e6f66076b703b3
Hermann Rasch <hermann.rasch93@gmail.com>
2024-04-10 18:48:40 +0200

    Fix incorrect routing path in Profile
    -Corrected the Link path on the title in the Profile Component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -196,11 +196,10 @@ export default function Profile() {
           <h1 className="text-center my-7 text-2xl font-semibold">Your Listings</h1>
           {userListings.map((listing)=> 
           <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
-            <Link to={`/lisitng/${listing._id}`}>
+            <Link to={`/listing/${listing._id}`}>
               <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain"/>
-
             </Link>
-            <Link to={`/lisitng/${listing.title}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate">
+            <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate">
               <p >{listing.name}</p>
             </Link>
             <div className="flex flex-col item-center"> 
